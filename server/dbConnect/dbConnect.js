99d477502adf4f34b848cb9a4548670f2d3e15be
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-27 08:58:46 +0000

    mongo setup

---
+++ server/dbConnect/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const dbConnect = () => {
+    try{
+        mongoose.connect(process.env.DB_URL)
+        console.log("Database has been connected without errors");
+        
+    } catch(error) {
+        console.log(error);
+        
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const helmet = require('helmet')
-const morgan = require('morgan')
+import express from 'express'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnect } from './dbConnect/dbConnect.js '
+
+dotenv.config()
 
 const app = express()
+
+app.listen(5000, () => {
+    console.log("Server is listening...");
+    dbConnect()
+})
