42660d7230a1f16b564f398a70eb3324b11d5006
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-27 09:21:56 +0000

    routes created

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import cors from 'cors'
 import { dbConnect } from './dbConnect/dbConnect.js '
+import routes from './routes/routes.js'
 
 
 dotenv.config()
@@ -13,6 +14,8 @@ app.use(helmet())
 app.use(morgan("common"))
 app.use(cors())
 app.use(express.json())
+
+app.use(routes)
 
 app.listen(5000, () => {
     console.log("Server is running...");
